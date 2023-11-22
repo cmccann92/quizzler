bfc1b1c6223b53fc8a147ffd03c8e78b1b6c2fe5
Connor <connorm@web.de>
2023-11-22 11:30:08 +0100

    setup models&admin models

--- quizzler/quiz_app/admin.py
+++ quizzler/quiz_app/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Question, Quiz, Score
 
-# Register your models here.
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ('text', 'correct_answer')
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ('title', 'category', 'difficulty')
+    filter_horizontal = ('questions',)
+
+@admin.register(Score)
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = ('player', 'quiz', 'score')
--- quizzler/quiz_app/models.py
+++ quizzler/quiz_app/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+class Question(models.Model):
+    text = models.CharField(max_length=255)
+    option_one = models.CharField(max_length=255)
+    option_two = models.CharField(max_length=255)
+    option_three = models.CharField(max_length=255)
+    option_four = models.CharField(max_length=255)
+    correct_answer = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.text
+    
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=255)
+    questions = models.ManyToManyField(Question)
+    category = models.CharField(max_length=255, blank=True)
+    difficulty = models.CharField(max_length=255, choices = [('Easy', 'Easy'), ('Medium', 'Medium'), ('Hard', 'Hard')])
+
+    def __str__(self):
+        return self.title
+
+class Score(models.Model):
+    player = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    score = models.PositiveBigIntegerField()
+
+    def __str__(self):
+        return f"{self.player}'s score on {self.quiz.title}: {self.score}"
+    
+    
