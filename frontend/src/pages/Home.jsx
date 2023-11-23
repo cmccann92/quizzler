ab9fd99bd6e87adcf0226b2cdc67b333a52ef84d
Marchuk Maxim <maxxik007@gmail.com>
2023-11-23 15:34:01 +0100

    [MOD]: React-router dom setup, creating Home page, justifying main project conf

---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import './Home.css';
+
+
+export default function Home() {
+	return( 
+		<div>
+			<section class="flex center-full first-section">
+				<h1>Welcome to Quizzler!</h1>
+			</section>
+		</div>
+	)
+}
