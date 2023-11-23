6c9b1d8bac55c782f99650e05a8b214098f01e68
Marchuk Maxim <maxxik007@gmail.com>
2023-11-23 14:28:21 +0100

    [MOD]: Adjusted settings.py

--- quizzler/quizzler/settings.py
+++ quizzler/quizzler/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": "quizzler",
-        "USER": "postgres",
+        "USER": os.getenv("DB_USER"),
         "PASSWORD": os.getenv("DB_PASSWORD"),
         "HOST": "127.0.0.1",
         "PORT": "5432",
@@ -114,7 +114,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Berlin'
 
 USE_I18N = True
 
